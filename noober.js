82dc6bbbdb55d16f873ea49ae3bb1a4a07cd2af0
Thomas Madsen <thomas.madsen@kellogg.northwestern.edu>
2021-01-27 23:11:44 -0600

    HW 3

--- noober.js
+++ noober.js
@@ -1,6 +1,60 @@
 function pageLoad() {
   // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
 
+numberOfRiders= ride.length
+
+passenger1Name = ride[0].passengerDetails.first + " " + ride[0].passengerDetails.last
+passenger1Phone = ride[0].passengerDetails.phoneNumber
+passenger1NumberOfPassengers = ride[0].numberOfPassengers
+passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+passenger1PickupAddressLine2 = ride[0].pickupLocation.city + " " + ride[0].pickupLocation.state + " " + ride[0].pickupLocation.zip
+passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + " " + ride[0].dropoffLocation.state + " " + ride[0].dropoffLocation.zip
+
+
+if (numberOfRiders >=2) {
+passenger2Name = ride[1].passengerDetails.first + " " + ride[1].passengerDetails.last
+passenger2Phone = ride[1].passengerDetails.phoneNumber
+passenger2NumberOfPassengers = ride[1].numberOfPassengers
+passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+passenger2PickupAddressLine2 = ride[1].pickupLocation.city + " " + ride[1].pickupLocation.state + " " + ride[1].pickupLocation.zip
+passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + " " + ride[1].dropoffLocation.state + " " + ride[1].dropoffLocation.zip
+
+}
+
+if (numberOfRiders >=3) {
+passenger3Name = ride[2].passengerDetails.first + " " + ride[2].passengerDetails.last
+passenger3Phone = ride[2].passengerDetails.phoneNumber
+passenger3NumberOfPassengers = ride[2].numberOfPassengers
+passenger3PickupAddressLine1 = ride[2].pickupLocation.address
+passenger3PickupAddressLine2 = ride[2].pickupLocation.city + " " + ride[2].pickupLocation.state + " " + ride[2].pickupLocation.zip
+passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
+passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + " " + ride[2].dropoffLocation.state + " " + ride[2].dropoffLocation.zip
+
+}
+
+purpleRequested = ride[0].purpleRequested
+
+if (purpleRequested == true){
+  levelOfService = "Noober Purple"
+}
+
+else if (numberOfRiders>1){
+  levelOfService = "Noober Pool"
+}
+
+else if (passenger1NumberOfPassengers > 3){
+  levelOfService = "Noober XL"
+}
+
+else {
+  levelOfService ="Noober X"
+}
+
+
+
+
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
   // the 'ride' variable contains data about a random ride (i.e. when the page)
   // is refreshed or reloaded, the 'ride' variable will contain something different
